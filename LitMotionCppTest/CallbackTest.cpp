a1ff83bbe0c93907d94889282a62630ff03e3a2d
Sadahiro Kenzo <sadahiro.kenzo+git@gmail.com>
2024-10-19 20:01:29 +0900

    Add Death Test. Add Throw Test.

--- LitMotionCppTest/CallbackTest.cpp
+++ LitMotionCppTest/CallbackTest.cpp
@@ -67,6 +67,7 @@ TEST_F(CallbackTest, Test_CreateOnCallback)
 	EXPECT_TRUE(completed);
 }
 
+#ifdef LIT_MOTION_CPP_ENABLE_EXCEPTION
 TEST_F(CallbackTest, Test_CompleteOnCallback_Self)
 {
 	try
@@ -103,7 +104,36 @@ TEST_F(CallbackTest, Test_CompleteOnCallback_CircularReference)
 		EXPECT_STREQ("Recursion of Complete call was detected.", exception.what());
 	}
 }
+#else
+TEST_F(CallbackTest, Test_CompleteOnCallback_Self)
+{
+	EXPECT_DEATH(
+		{
+			MotionHandle handle;
+			handle = LMotion::create(0.0f, 10.0f, 1.0f)
+				.withOnComplete([&handle]() {handle.complete(); })
+				.runWithoutBinding();
+			handle.complete();
+		}, "");
+}
 
+TEST_F(CallbackTest, Test_CompleteOnCallback_CircularReference)
+{
+	EXPECT_DEATH(
+		{
+			MotionHandle handle1;
+			MotionHandle handle2;
+			handle1 = LMotion::create(0.0f, 10.0f, 1.0f)
+				.withOnComplete([&handle2]() {handle2.complete(); })
+				.runWithoutBinding();
+			handle2 = LMotion::create(0.0f, 10.0f, 1.0f)
+				.withOnComplete([&handle1]() {handle1.complete(); })
+				.runWithoutBinding();
+
+			handle1.complete();
+		}, "");
+}
+#endif
 TEST_F(CallbackTest, Test_CompleteOnCallback_Other)
 {
 	auto otherHandle = LMotion::create(0.0f, 10.0f, 5.0f).runWithoutBinding();
@@ -117,6 +147,7 @@ TEST_F(CallbackTest, Test_CompleteOnCallback_Other)
 	EXPECT_FALSE(otherHandle.isActive());
 }
 
+#ifdef LIT_MOTION_CPP_ENABLE_EXCEPTION
 TEST_F(CallbackTest, Test_ThorwExceptionInsideCallbck)
 {
 	LMotion::create(0.0f, 10.0f, 0.5f)
@@ -174,3 +205,89 @@ TEST_F(CallbackTest, Test_RegisterUnhandledExceptionHandler)
 
 	MotionDispatcher::registerUnhandledExceptionHandler(defaultHandler);
 }
+#else
+TEST_F(CallbackTest, Test_ThorwExceptionInsideCallbck)
+{
+	EXPECT_THROW(
+		{
+			LMotion::create(0.0f, 10.0f, 0.5f)
+				.withOnComplete([]() {throw std::exception{ "Test" }; })
+				.runWithoutBinding();
+
+			ManualMotionDispatcher::update(0.6f);
+			EXPECT_STREQ("Exception: Test", MotionDispatcher::getLastError());
+		}, std::exception
+	);
+}
+
+TEST_F(CallbackTest, Test_ThorwExceptionInsideCallbck_ThenCompleteManually)
+{
+	EXPECT_THROW(
+		{
+			auto handle = LMotion::create(0.0f, 10.0f, 0.5f)
+				.withOnComplete([]() {throw std::exception{ "Test" }; })
+				.runWithoutBinding();
+
+			handle.complete();
+			EXPECT_STREQ("Exception: Test", MotionDispatcher::getLastError());
+		},std::exception
+	);
+}
+
+TEST_F(CallbackTest, Test_WithCancelOnError)
+{
+	EXPECT_THROW(
+		{
+			bool completed = false;
+			LMotion::create(0.0f, 10.0f, 0.5f)
+				.withCancelOnError()
+				.withOnComplete([&completed]() {completed = true; })
+				.bind([](float x) {throw std::exception("Test"); });
+
+			ManualMotionDispatcher::update(0.7f);
+			EXPECT_FALSE(completed);
+		}, std::exception
+	);
+}
+
+TEST_F(CallbackTest, Test_ThorwExceptionInsideBind_ThenCompleteManually)
+{
+	EXPECT_THROW(
+		{
+			auto handle = LMotion::create(0.0f, 10.0f, 0.5f)
+				.bind([](float x) {throw std::exception("Test"); });
+
+			handle.complete();
+			EXPECT_STREQ("Exception: Test", MotionDispatcher::getLastError());
+		}, std::exception
+	);
+}
+
+TEST_F(CallbackTest, Test_RegisterUnhandledExceptionHandler)
+{
+	EXPECT_THROW(
+		{
+			std::string lastError;
+
+			auto defaultHandler = MotionDispatcher::getUnhandledExceptionHandler();
+			MotionDispatcher::registerUnhandledExceptionHandler([&lastError](std::exception& exception) {lastError = exception.what(); });
+			try
+			{
+				LMotion::create(0.0f, 10.0f, 0.5f)
+					.withOnComplete([]() {throw std::exception{ "Test" }; })
+					.runWithoutBinding();
+
+				ManualMotionDispatcher::update(0.7f);
+				EXPECT_STREQ("Test", lastError.c_str());
+
+				MotionDispatcher::registerUnhandledExceptionHandler(defaultHandler);
+			}
+			catch (std::exception& exception)
+			{
+				MotionDispatcher::registerUnhandledExceptionHandler(defaultHandler);
+				throw;
+			}
+		}, std::exception
+	);
+}
+#endif
